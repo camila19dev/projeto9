e4ba18f43fbbda14502f56e888c980ec394cb134
camila19dev <101355779+camila19dev@users.noreply.github.com>
2022-03-10 14:57:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200, 200, 30, 30);
+  box.shapeColor = "yellow";
+}
+
+function draw() {
+  background(30);
+drawSprites();
+
+if (keyDown(RIGHT_ARROW)) {
+box.position.x = box.position.x +5;
+}
+
+if (keyDown(LEFT_ARROW))  {
+box.position.x = box.position.x -5;
+}
+
+if (keyDown(DOWN_ARROW))  {
+box.position.y = box.position.y +5;
+}
+
+if (keyDown(UP_ARROW))    {
+box.position.y = box.position.y -5;
+
+}
+}
